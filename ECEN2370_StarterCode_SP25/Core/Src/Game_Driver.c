f5af77ad0a517e44f607dae95d4d12c330c287cd
RenePunch <121848199+RenePunch@users.noreply.github.com>
2025-04-22 13:43:59 -0600

    new game code

--- ECEN2370_StarterCode_SP25/Core/Src/Game_Driver.c
+++ ECEN2370_StarterCode_SP25/Core/Src/Game_Driver.c
@@ -2,123 +2,76 @@
 #include <stdlib.h>
 #include "Game_Driver.h"
 
-int board[ROWS][COLS];
-int currentPlayer;
-int selectedCol;
-bool gameOver;
+char board[ROWS][COLS];
 
 
-void gfxInit(void) {  }
-void gfxClear(void) {  }
-void gfxDrawBoard(void) {
-    for (int r = 0; r < ROWS; ++r) {
-        for (int c = 0; c < COLS; ++c) {
-            int color = (board[r][c] == 0 ? 0 :
-                         board[r][c] == 1 ? PLAYER1_COLOR : PLAYER2_COLOR);
-            
+void initBoard() {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            board[r][c] = ' ';
         }
     }
 }
-void gfxDrawHoverCoin(void) {
-    int color = (currentPlayer == 1 ? PLAYER1_COLOR : PLAYER2_COLOR);
-   
-}
-
-bool touchAvailable(void) { return false; }
-void getTouch(int *x, int *y) { *x = 0; *y = 0; }
-bool dropButtonPressed(int tx, int ty) {
-    return (tx >= DROP_BUTTON_X && tx <= DROP_BUTTON_X + DROP_BUTTON_WIDTH &&
-            ty >= DROP_BUTTON_Y && ty <= DROP_BUTTON_Y + DROP_BUTTON_HEIGHT);
-}
 
 
-void initBoard(void) {
-    for (int r = 0; r < ROWS; ++r)
-        for (int c = 0; c < COLS; ++c)
-            board[r][c] = 0;
-    currentPlayer = 1;
-    selectedCol = 0;
-    gameOver = false;
+void printBoard() {
+    printf("\n");
+    for (int r = 0; r < ROWS; r++) {
+        printf("|");
+        for (int c = 0; c < COLS; c++) {
+            printf(" %c |", board[r][c]);
+        }
+        printf("\n");
+    }
+    printf("-----------------------------\n");
+    printf("  1   2   3   4   5   6   7  \n\n");
 }
 
-bool isColumnFull(int col) {
-    return board[0][col] != 0;
-}
-bool dropCoin(int col) {
-    if (isColumnFull(col)) return false;
-    for (int r = ROWS - 1; r >= 0; --r) {
-        if (board[r][col] == 0) {
-            board[r][col] = currentPlayer;
-            return true;
+
+int dropPiece(int col, char piece) {
+    if (col < 0 || col >= COLS) return -1;
+    for (int r = ROWS - 1; r >= 0; r--) {
+        if (board[r][col] == ' ') {
+            board[r][col] = piece;
+            return r;
         }
     }
-    return false;
+    return -1; 
 }
 
-bool checkDirection(int startR, int startC, int dR, int dC) {
-    for (int i = 0; i < 4; ++i) {
+
+int checkDirection(int startR, int startC, int dR, int dC, char piece) {
+    for (int i = 1; i < 4; i++) {
         int r = startR + dR * i;
         int c = startC + dC * i;
-        if (r < 0 || r >= ROWS || c < 0 || c >= COLS || board[r][c] != currentPlayer)
-            return false;
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS || board[r][c] != piece)
+            return 0;
     }
-    return true;
+    return 1;
 }
 
-bool checkWin(void) {
-    for (int r = 0; r < ROWS; ++r) {
-        for (int c = 0; c < COLS; ++c) {
-            if (board[r][c] != currentPlayer) continue;
-            if (c <= COLS - 4 && checkDirection(r, c, 0, 1)) return true;
-            if (r <= ROWS - 4 && checkDirection(r, c, 1, 0)) return true;
-            if (r <= ROWS - 4 && c <= COLS - 4 && checkDirection(r, c, 1, 1)) return true;
-            if (r <= ROWS - 4 && c >= 3 && checkDirection(r, c, 1, -1)) return true;
-        }
-    }
-    return false;
-}
 
-void switchPlayer(void) {
-    currentPlayer = (currentPlayer == 1 ? 2 : 1);
-}
-
-int main(void) {
-    gfxInit();
-    initBoard();
-
-    while (!gameOver) {
-        gfxClear();
-        gfxDrawBoard();
-        gfxDrawHoverCoin();
-
-        if (touchAvailable()) {
-            int tx, ty;
-            getTouch(&tx, &ty);
-
-            
-            if (ty < SCREEN_HEIGHT - DROP_BUTTON_HEIGHT) {
-                if (tx < SCREEN_WIDTH / 2) {
-                    if (selectedCol > 0) selectedCol--;
-                } else {
-                    if (selectedCol < COLS - 1) selectedCol++;
-                }
-            }
-
-       
-            if (dropButtonPressed(tx, ty)) {
-                if (dropCoin(selectedCol)) {
-                    if (checkWin()) {
-                        gameOver = true; 
-                    } else {
-                        switchPlayer();
-                    }
-                }
+int checkWin(char piece) {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            if (board[r][c] != piece) continue;
+           
+            if (checkDirection(r, c, 0, 1, piece) ||
+                checkDirection(r, c, 1, 0, piece) ||
+                checkDirection(r, c, 1, 1, piece) ||
+                checkDirection(r, c, -1, 1, piece)) {
+                return 1;
             }
         }
-
-        
-
     }
-
     return 0;
 }
+
+
+int isBoardFull() {
+    for (int r = 0; r < ROWS; r++)
+        for (int c = 0; c < COLS; c++)
+            if (board[r][c] == ' ') return 0;
+    return 1;
+}
+
---
+++ ECEN2370_StarterCode_SP25/Debug/Core/Src/Game_Driver.d
@@ -0,0 +1,3 @@
+Core/Src/Game_Driver.o: ../Core/Src/Game_Driver.c \
+ ../Core/Inc/Game_Driver.h
+../Core/Inc/Game_Driver.h:
